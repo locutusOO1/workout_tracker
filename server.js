643fd671333aead1bec09204cc5698ac7d13ad97
Clark Kent with glasses <61812163+locutusOO1@users.noreply.github.com>
2020-08-30 18:14:34 -0500

    added html routes

--- server.js
+++ server.js
@@ -19,7 +19,15 @@ app.use(express.static("public"));
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
 
 app.get("/", (req,res) => {
-    res.sendFile(path.join(__dirname + "./public/index.html"))
+    res.sendFile(path.join(__dirname,"public","index.html"));
+});
+
+app.get("/stats", (req,res) => {
+    res.sendFile(path.join(__dirname,"public","stats.html"));
+});
+
+app.get("/exercise", (req,res) => {
+    res.sendFile(path.join(__dirname,"public","exercise.html"));
 });
 
 // put routes here
