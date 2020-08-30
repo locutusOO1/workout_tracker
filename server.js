13febca51b28a0e77e697cc6226f2d9bd49b7d97
Clark Kent with glasses <61812163+locutusOO1@users.noreply.github.com>
2020-08-30 17:46:13 -0500

    added root path

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
+const path = require("path")
 
 const PORT = process.env.PORT || 3000;
 
@@ -17,14 +18,18 @@ app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
 
+app.get("/", (req,res) => {
+    res.sendFile(path.join(__dirname + "./public/index.html"))
+});
+
 // put routes here
-db.Workout.create({})
-    .then(dbWorkout => {
-        console.log(dbWorkout);
-    })
-    .catch(({ message }) => {
-        console.log(message);
-    });
+// db.Workout.create({})
+//     .then(dbWorkout => {
+//         console.log(dbWorkout);
+//     })
+//     .catch(({ message }) => {
+//         console.log(message);
+//     });
 
 app.listen(PORT, () => {
     console.log(`App running http://localhost:${PORT}`);
