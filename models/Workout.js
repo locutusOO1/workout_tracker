397b857e9f761e8e9c739afc2b2a23122264f6d1
Clark Kent with glasses <61812163+locutusOO1@users.noreply.github.com>
2020-09-04 15:09:24 -0500

    deploying

--- models/Workout.js
+++ models/Workout.js
@@ -5,7 +5,7 @@ const Schema = mongoose.Schema;
 const WorkoutSchema = new Schema({
     day: {
         type: Date,
-        default: Date.now
+        default: Date.now()
     }
     ,
     exercises: {
